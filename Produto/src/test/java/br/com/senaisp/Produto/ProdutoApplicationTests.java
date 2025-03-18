348be48199b1b323eb33ff577ab7615b8b482c16
ThiagoNascBarros <thiago171nasciment@gmail.com>
2025-03-17 21:44:54 -0300

    feat(produto) Adiciona validações aos atributos
    Signed-off-by: ThiagoNascBarros <thiago171nasciment@gmail.com>

---
+++ Produto/src/test/java/br/com/senaisp/Produto/ProdutoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.senaisp.Produto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
