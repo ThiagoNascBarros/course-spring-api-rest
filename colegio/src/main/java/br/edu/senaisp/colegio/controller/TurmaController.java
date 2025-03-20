5d761958aff02193961c33887c1cd8fd961e1682
ThiagoNascBarros <thiago171nasciment@gmail.com>
2025-03-19 21:52:11 -0300

    feat(TurmaController, TurmaService): Criando metodos GET e DELETE
    Signed-off-by: ThiagoNascBarros <thiago171nasciment@gmail.com>

--- colegio/src/main/java/br/edu/senaisp/colegio/controller/TurmaController.java
+++ colegio/src/main/java/br/edu/senaisp/colegio/controller/TurmaController.java
@@ -1,7 +1,13 @@
 package br.edu.senaisp.colegio.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,18 +22,53 @@ public class TurmaController {
 
 	@Autowired
 	private TurmaService turmaService;
-	
-	@PostMapping
-	public ResponseEntity inserir(@RequestBody Turma turma) {
-		
+
+	@GetMapping
+	public ResponseEntity buscarTodos(BindingResult bind) {
 		try {
-			Turma a = turmaService.gravarTurma(turma);
-			
-			return ResponseEntity.status(200).body(turma);
+			List<Turma> turmas = turmaService.exibirTurmas();
+			if (!turmas.isEmpty())
+				return ResponseEntity.status(200).body(turmas);
+			else
+				return ResponseEntity.status(500).body(bind.getAllErrors());
 		} catch (Exception e) {
 			return ResponseEntity.status(400).body(e.getMessage());
 		}
-		
+	}
+
+	@GetMapping("{id}")
+	public ResponseEntity buscarUmaTurma(@PathVariable Long id) {
+		try {
+			Turma turma = turmaService.exibirUmaTurma(id);
+			if (turma == null)
+				return ResponseEntity.status(404).build();
+			else
+				return ResponseEntity.status(200).body(turma);
+		} catch (Exception e) {
+			return ResponseEntity.status(500).body(e);
+		}
+	}
+
+	@PostMapping
+	public ResponseEntity inserir(@RequestBody Turma turma) {
+		try {
+			return ResponseEntity.status(201).body(turmaService.gravarTurma(turma));
+		} catch (Exception e) {
+			return ResponseEntity.status(400).body(e.getMessage());
+		}
 	}
 	
+	@DeleteMapping("{id}")
+	public ResponseEntity deletarTurma(@PathVariable Long id) {
+		try {
+			Turma turma = turmaService.excluirPorId(id);
+			if (turma == null)
+				return ResponseEntity.status(404).build();
+			else
+				return ResponseEntity.status(200).body(turma);
+		} catch (Exception e) {
+			return ResponseEntity.status(400).body(e.getMessage());
+		}
+	}
+
 }
--- colegio/src/main/java/br/edu/senaisp/colegio/service/TurmaService.java
+++ colegio/src/main/java/br/edu/senaisp/colegio/service/TurmaService.java
@@ -1,5 +1,10 @@
 package br.edu.senaisp.colegio.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import javax.management.RuntimeErrorException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +19,29 @@ public class TurmaService {
 	
 	public Turma gravarTurma(Turma t) {
 		return repo.save(t);
+	}
+	
+	public List<Turma> exibirTurmas(){
+		return repo.findAll();
+	}
+	
+	public Turma exibirUmaTurma(Long id) {
+		Optional<Turma> turma = repo.findById(id);
+//					 Se der errado retornar os erros
+		return turma.orElse(null);
+	}
+	
+	public Turma excluirPorId(Long id) {
+		try {
+			Turma turma = exibirUmaTurma(id);
+			if(turma != null) {
+				repo.deleteById(id);
+				return turma;
+			}
+		} catch (Exception e) {
+			throw new RuntimeException("Error: " + e.getMessage());
+		}
+		return null;
 	}
 
 }
